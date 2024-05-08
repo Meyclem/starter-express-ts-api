7870726b1d228eae21aaf0493655aa9790b53d73
Clement Meyer <meyclem@gmail.com>
2024-05-08 18:44:21 +0200

    update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from eslint-plugin-prettier/recommended
+
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": 1,
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+  ...eslintPluginPrettierRecommended
+});
