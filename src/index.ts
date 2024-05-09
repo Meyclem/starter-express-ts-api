14a3d250aa3466046f0cac11955e0a88ced12b7b
Clement <meyclem@gmail.com>
2024-05-09 15:12:02 +0200

    Fix build (#2)
    This PR fixes the build command because of a wrong and outdated configuration.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import server from "./server";
+import dotenv from "dotenv";
+import server from "./server.js";
 
-const PORT = 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`🤖 Server is running on http://localhost:${PORT}`);
