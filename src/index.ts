b3fec619cfd751c74fe00a4378b216d7ffbface7
Clement Meyer <meyclem@gmail.com>
2024-05-09 14:27:51 +0200

    remove ts-node + nodemon and replace with tsx

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import server from "./server";
 const PORT = 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`🤖 Server is running on http://localhost:${PORT}`);
 });
